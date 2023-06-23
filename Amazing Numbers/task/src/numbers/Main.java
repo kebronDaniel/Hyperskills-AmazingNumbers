2079b5a89c8d0c33ca4453f87d585b2354e1eaa0
KebronDaniel <kebronjrdaniel@gmail.com>
2023-06-23 16:18:44 +0300

    Finished the spy numbers challenge. closes #5.

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -28,25 +28,7 @@ public class Main {
                     }
 
                     if (checkNaturalNumber(number)){
-                        System.out.println();
-                        System.out.println("Properties of " + number);
-
-                        boolean buzz = checkBuzz(number);
-                        System.out.println("        buzz:   "+buzz);
-
-                        boolean duck = checkDuck(numberInput[0]);
-                        System.out.println("        duck:   " + duck);
-
-                        boolean palindromic = checkPalindromic(numberInput[0]);
-                        System.out.println(" palindromic:   " + palindromic);
-
-                        boolean gapful = checkGapfulNumbers(numberInput[0]);
-                        System.out.println("      gapful:   " + gapful);
-
-                        boolean even = numberIsEven(number);
-                        System.out.println("        even:   " + even);
-                        System.out.println("         odd:   " + !even);
-                        System.out.println();
+                        showPropertyInMultipleLine(number);
 
                     } else {
                         System.out.println();
@@ -55,8 +37,8 @@ public class Main {
                     }
                 }catch (NumberFormatException e){
                     System.out.println();
-                    System.out.println("The first parameter should be a natural number or zero.");
-                    System.out.println();
+                    System.out.println("The property ["+ numberInput[0] +"] is wrong.");
+                    System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY]");
                 }
 
             }
@@ -67,30 +49,34 @@ public class Main {
                     long secondNumber = Long.parseLong(numberInput[1]);
 
                     if (checkNaturalNumber(firstNumber) && (checkNaturalNumber(secondNumber))){
-                        for (int i = 0; i < secondNumber; i++) {
-                            System.out.print(firstNumber+ " is ");
-
-                            if (checkBuzz(firstNumber)){
-                                System.out.print("Buzz,  ");
-                            }
-                            if (checkDuck(String.valueOf(firstNumber))){
-                                System.out.print("duck,  ");
-                            }
-                            if (checkPalindromic(String.valueOf(firstNumber))){
-                                System.out.print("palindromic, ");
-                            }
-                            if (checkGapfulNumbers(String.valueOf(firstNumber))){
-                                System.out.print("gapful,  ");
-                            }
-                            if (numberIsEven(firstNumber)){
-                                System.out.println("even");
-                            }
-                            if (!numberIsEven(firstNumber)){
-                                System.out.println("odd");
-                            }
-                            System.out.println();
-                            firstNumber++;
-                        }
+//                        for (int i = 0; i < secondNumber; i++) {
+//                            System.out.print(firstNumber+ " is ");
+//
+//                            if (checkBuzz(firstNumber)){
+//                                System.out.print("Buzz,  ");
+//                            }
+//                            if (checkDuck(String.valueOf(firstNumber))){
+//                                System.out.print("duck,  ");
+//                            }
+//                            if (checkPalindromic(String.valueOf(firstNumber))){
+//                                System.out.print("palindromic, ");
+//                            }
+//                            if (checkGapfulNumbers(String.valueOf(firstNumber))){
+//                                System.out.print("gapful,  ");
+//                            }
+//                            if (checkSpy(String.valueOf(firstNumber))){
+//                                System.out.print("spy,  ");
+//                            }
+//                            if (numberIsEven(firstNumber)){
+//                                System.out.println("even");
+//                            }
+//                            if (!numberIsEven(firstNumber)){
+//                                System.out.println("odd");
+//                            }
+//                            System.out.println();
+//                            firstNumber++;
+//                        }
+                        showPropertyInRange(firstNumber,secondNumber);
                     }else {
                         if (!checkNaturalNumber(firstNumber)){
                             System.out.println();
@@ -109,21 +95,171 @@ public class Main {
                         long firstNumber = Long.parseLong(numberInput[0]);
                     }catch (NumberFormatException x){
                         System.out.println();
-                        System.out.println("The first parameter should be a natural number or zero.");
-                        System.out.println();
+                        System.out.println("The property ["+ numberInput[0] +"] is wrong.");
+                        System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY]");
                     }
                     try {
                         long secondNumber = Long.parseLong(numberInput[1]);
                     }catch (NumberFormatException x){
                         System.out.println();
-                        System.out.println("second parameter should be a natural number.");
-                        System.out.println();
+                        System.out.println("The property ["+ numberInput[1] +"] is wrong.");
+                        System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY]");
                     }
                 }
             }
+            if (numberInput.length == 3){
 
+                try {
+                    long firstNumber = Long.parseLong(numberInput[0]);
+                    long secondNumber = Long.parseLong(numberInput[1]);
+                    String property = numberInput[2].toLowerCase();
+
+                    if (firstNumber <= 0){
+                        System.out.println("The first parameter should be a natural number or zero.");
+                        System.out.println();
+                    } else if (secondNumber <= 0) {
+                        System.out.println("The second parameter should be a natural number or zero.");
+                        System.out.println();
+                    } else {
+                        getPropertyNTimes(firstNumber,secondNumber,property);
+                    }
+
+
+
+                }catch (NumberFormatException numberFormatException){
+                    try {
+                        long firstNumber = Long.parseLong(numberInput[0]);
+                    }catch (NumberFormatException firstNumberException){
+                        System.out.println();
+                        System.out.println("The first parameter should be a natural number or zero.");
+                    }
+                    try {
+                        long secondNumber = Long.parseLong(numberInput[1]);
+                    }catch (NumberFormatException firstNumberException){
+                        System.out.println();
+                        System.out.println("The second parameter should be a natural number or zero.");
+                    }
+                }
+
+            }
         }
 
+    }
+
+    private static void showPropertyInMultipleLine(long number) {
+        System.out.println();
+        System.out.println("Properties of " + number);
+
+        boolean buzz = checkBuzz(number);
+        System.out.println("        buzz:   "+buzz);
+
+        boolean duck = checkDuck(String.valueOf(number));
+        System.out.println("        duck:   " + duck);
+
+        boolean palindromic = checkPalindromic(String.valueOf(number));
+        System.out.println(" palindromic:   " + palindromic);
+
+        boolean gapful = checkGapfulNumbers(String.valueOf(number));
+        System.out.println("      gapful:   " + gapful);
+
+        boolean spy = checkSpy(String.valueOf(number));
+        System.out.println("         spy:   " + spy);
+
+        boolean even = numberIsEven(number);
+        System.out.println("        even:   " + even);
+        System.out.println("         odd:   " + !even);
+        System.out.println();
+    }
+
+    public static void showPropertyInOneLine(long number){
+        System.out.print(number + " is ");
+        if (checkBuzz(number)){
+            System.out.print("buzz,  ");
+        }
+        if (checkDuck(String.valueOf(number))) {
+            System.out.print("duck,  ");
+        }
+        if (checkPalindromic(String.valueOf(number))) {
+            System.out.print("palindromic,  ");
+        }
+        if (checkGapfulNumbers(String.valueOf(number))) {
+            System.out.print("gapful,  ");
+        }
+        if (checkSpy(String.valueOf(number))) {
+            System.out.print("spy,  ");
+        }
+        if (numberIsEven(number)) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+
+    public static void showPropertyInRange(long firstNumber, long secondNumber){
+        for (int i = 0; i < secondNumber; i++) {
+            showPropertyInOneLine(firstNumber);
+            firstNumber++;
+        }
+    }
+
+    public static void getPropertyNTimes(long firstNumber, long secondNumber, String property){
+        long counter = 0;
+
+        while (true){
+
+            if (property.equals("buzz")){
+                if (checkBuzz(firstNumber)){
+                    showPropertyInOneLine(firstNumber);
+                    counter++;
+                }
+                firstNumber++;
+            } else if (property.equals("duck")) {
+                if (checkDuck(String.valueOf(firstNumber))){
+                    showPropertyInOneLine(firstNumber);
+                    counter++;
+                }
+                firstNumber++;
+            } else if (property.equals("palindromic")) {
+                if (checkPalindromic(String.valueOf(firstNumber))){
+                    showPropertyInOneLine(firstNumber);
+                    counter++;
+                }
+                firstNumber++;
+            } else if (property.equals("gapful")) {
+                if (checkGapfulNumbers(String.valueOf(firstNumber))){
+                    showPropertyInOneLine(firstNumber);
+                    counter++;
+                }
+                firstNumber++;
+            } else if (property.equals("spy")) {
+                if (checkSpy(String.valueOf(firstNumber))){
+                    showPropertyInOneLine(firstNumber);
+                    counter++;
+                }
+                firstNumber++;
+            } else if (property.equals("even")) {
+                if (numberIsEven(firstNumber)){
+                    showPropertyInOneLine(firstNumber);
+                    counter++;
+                }
+                firstNumber++;
+            } else if (property.equals("odd")) {
+                if (!numberIsEven(firstNumber)){
+                    showPropertyInOneLine(firstNumber);
+                    counter++;
+                }
+                firstNumber++;
+            } else {
+                System.out.println();
+                System.out.println("The property ["+property+"] is wrong");
+                System.out.println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD]");
+                break;
+            }
+
+            if(counter == secondNumber){
+                break;
+            }
+        }
     }
 
     private static void showInfo() {
@@ -134,6 +270,7 @@ public class Main {
         System.out.println("- enter two natural numbers to obtain the properties of the list:");
         System.out.println("  * the first parameter represents a starting number;");
         System.out.println("  * the second parameter shows how many consecutive numbers are to be processed;");
+        System.out.println("- two natural numbers and a property to search for;");
         System.out.println("- separate the parameters with one space;");
         System.out.println("- enter 0 to exit.");
         System.out.println();
@@ -149,13 +286,23 @@ public class Main {
 
     public static boolean checkGapfulNumbers(String userInput){
         String firstAndLastDigit = Character.toString(userInput.charAt(0)) + Character.toString(userInput.charAt(userInput.length() - 1));
-        return ((userInput.length() > 3) && (Long.parseLong(userInput) % Long.parseLong(firstAndLastDigit) == 0)) ? true : false;
+        return ((userInput.length() >= 3) && (Long.parseLong(userInput) % Long.parseLong(firstAndLastDigit) == 0)) ? true : false;
     }
 
     public static boolean checkPalindromic(String number){
         StringBuilder stringBuilder = new StringBuilder(number);
         String reversedNumber = stringBuilder.reverse().toString();
         return (number.equals(reversedNumber)) ? true : false;
+    }
+
+    public static boolean checkSpy(String number){
+        long sum = 0;
+        long product = 1;
+        for (int i = 0; i < number.length(); i++) {
+            sum += Long.parseLong(String.valueOf(number.charAt(i)));
+            product *= Long.parseLong(String.valueOf(number.charAt(i)));
+        }
+        return (sum == product) ? true: false;
     }
 
     public static boolean checkDuck(String number){
---
+++ Topics/Debugging techniques/Find the error/src/terst.java
@@ -0,0 +1,8 @@
+public class terst {
+
+    private String name;
+    public terst(String name) {
+        assert (name == null):"Name can not be null";
+        this.name = name;
+    }
+}
